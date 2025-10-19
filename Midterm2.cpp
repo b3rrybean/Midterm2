99a613e3172dce746150a330b5a8a745e9101469
Nicole C. <nmcanson@gmail.com>
2025-10-19 00:17:08 -0700

    Initial commit

---
+++ Midterm2.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        string data;
+        Node* prev;
+        Node* next;
+
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void push_back(string v) {
+        Node* newNode = new Node(v);
+        if (!tail)
+            head = tail = newNode;
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void push_front(string v) {
+        Node* newNode = new Node(v);
+        if (!head)
+            head = tail = newNode;
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void pop_front() {
+        if (!head) return;
+        Node* temp = head;
+        if (head->next) {
+            head = head->next;
+            head->prev = nullptr;
+        } else
+            head = tail = nullptr;
+
+        delete temp;
+    }
+
+    void pop_back() {
+        if (!tail) return;
+        Node* temp = tail;
+        if (tail->prev) {
+            tail = tail->prev;
+            tail->next = nullptr;
+        } else
+            head = tail = nullptr;
+
+        delete temp;
+    }
+
+    ~DoublyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void print() {
+        Node* current = head;
+        if (!current) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void print_reverse() {
+        Node* current = tail;
+        if (!current) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        while (current) {
+            cout << current->data << " ";
+            current = current->prev;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS; // dummy statement to avoid compiler warning
+    return 0;
+}
